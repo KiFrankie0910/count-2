d7f27be21b408641411291d63270f8fd9d493ea4
Ki Frankie <106165688+kifrankie0910@users.noreply.github.com>
2023-01-06 15:41:44 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
+    Count += 1
+    basic.showNumber(Count)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.clearScreen()
+    Count = 0
+    basic.showNumber(Count)
 })
 let Count = 0
+Count = 0
